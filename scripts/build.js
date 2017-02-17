3b7b2dbf31e27ef87124638bd6b4cb5af05ca32c
Ignacio Vivona <ignaciovivona@softwaremissinglink.com>
2017-02-17 16:25:48 -0300

    Directories

---
+++ scripts/build.js
@@ -0,0 +1,4 @@
+var webpack = require('webpack')
+var config = require('../node_modules/react-scripts/config/webpack.config.prod.js')
+
+console.log(config)
---
+++ scripts/static.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import FS from 'fs'
+import App from '../src/App.js'
+
+console.log(App)
