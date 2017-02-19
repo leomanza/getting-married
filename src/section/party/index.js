558846759c2f4bf9b63c7f7059a38fa69bf4f164
Ignacio Vivona <ignaciovivona@softwaremissinglink.com>
2017-02-19 17:58:36 -0300

    Noopener

--- src/section/party/index.js
+++ src/section/party/index.js
@@ -11,7 +11,7 @@ const Party = () => (
       color: 'white'
     }}>
     <Title icon={PartyIcon} white>¡La Fiesta!</Title>
-    <h2 style={h2}>Como dijimos al principio, lo importate ocurre el <u>Sábado 4 de Noviembre </u>a las <u>20 horas</u> en <a href="https://www.google.com.ar/maps/place/Juana+Manso+1548,+Cdad.+Aut%C3%B3noma+de+Buenos+Aires/@-34.6151732,-58.3625643,19z/data=!3m1!4b1!4m5!3m4!1s0x95a334d7158b30db:0x971bf8e3deae61f6!8m2!3d-34.6151732!4d-58.3620171" target="_blank" style={{ color: 'white', textDecoration: 'none' }}><u>Juana Manso 1548</u></a>.</h2>
+    <h2 style={h2}>Como dijimos al principio, lo importate ocurre el <u>Sábado 4 de Noviembre </u>a las <u>20 horas</u> en <a rel="noopener" href="https://www.google.com.ar/maps/place/Juana+Manso+1548,+Cdad.+Aut%C3%B3noma+de+Buenos+Aires/@-34.6151732,-58.3625643,19z/data=!3m1!4b1!4m5!3m4!1s0x95a334d7158b30db:0x971bf8e3deae61f6!8m2!3d-34.6151732!4d-58.3620171" target="_blank" style={{ color: 'white', textDecoration: 'none' }}><u>Juana Manso 1548</u></a>.</h2>
   </Section>
 )
 
