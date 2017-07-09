e0b75f8cfd61ffd5c05c677035077bdce79cbf03
Ignacio Vivona <ignaciovivona@softwaremissinglink.com>
2017-07-09 00:51:24 -0300

    mas fotos

--- src/section/dress-code/index.js
+++ src/section/dress-code/index.js
@@ -5,7 +5,7 @@ import { h2 } from '../Styles'
 import DressCodeIcon from '../../icons/BowTies.svg'
 
 export const DressCode = () => (
-  <Section>
+  <Section style={{ color: '#343a40' }}>
     <Title icon={DressCodeIcon}>La Vestimenta</Title>
     <h2 style={h2}>Nos olvidamos de la formalidad, pueden venir vestidos como quieran.</h2>
   </Section>
